cc4c553ea928e6811828b1dae052a86d6ab6887f
alimehdi <alimehdi9651@gmail.com>
2024-08-10 17:53:51 +0530

    first commit

---
+++ main/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from .import views
+
+
+urlpatterns = [
+    path('', views.index, name = 'index'),
+    path('c/login', views.login_view, name = 'login'),
+    path('c/register', views.register_view, name = 'register'),
+    path('c/dashboard', views.dashboard_view, name = 'dashboard'),
+    path('s/login', views.slogin_view, name = 'seller_login'),
+    path('s/register', views.sregister_view, name = 'seller_dashboard'),
+    path('s/dashboard', views.sdashboard_view, name = 'seller_dashboard'),
+    path('logout', views.logout_view, name = 'logout'),
+
+    
+]
