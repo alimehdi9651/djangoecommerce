29f62bb0faf05e63fd6f5a6c4d95571174fcaba7
alimehdi <alimehdi9651@gmail.com>
2024-08-12 01:59:12 +0530

    setup

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .import views
+from main import views
 
 
 urlpatterns = [
@@ -11,7 +11,5 @@ urlpatterns = [
     path('s/login', views.slogin_view, name = 'seller_login'),
     path('s/register', views.sregister_view, name = 'seller_dashboard'),
     path('s/dashboard', views.sdashboard_view, name = 'seller_dashboard'),
-    path('logout', views.logout_view, name = 'logout'),
-
-    
+    path('logout', views.logout_view, name = 'logout'),   
 ]
